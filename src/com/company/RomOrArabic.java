b90352a671f414d3d68b6f3de1ee8c0ee7392929
Java488-oss <59206612+Java488-oss@users.noreply.github.com>
2020-01-16 14:41:31 +0600

    commit

--- src/com/company/RomOrArabic.java
+++ src/com/company/RomOrArabic.java
@@ -4,7 +4,6 @@ public class RomOrArabic extends Main {
    private static int numbers[]  = {1, 4, 5, 9, 10, 50, 100, 500, 1000 };
 
    private static String letters[]  = { "I", "IV", "V", "IX", "X", "L", "C", "D", "M"};
-   static int ss;
 
     public static void convertIntegerToRoman(int number)
     {
